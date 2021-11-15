143ef40ec2588aaed20280898c5a3bfb2d7cc2cd
CekanLucas <cekanlucas@gmail.com>
2021-11-15 01:13:41 -0500

    used a online template for the library project

--- JSprograms/Odin/Library/library.js
+++ JSprograms/Odin/Library/library.js
@@ -1,9 +1,39 @@
-let myLibrary = [];
-
-function Book() {
-  // the constructor...
+var initialLibrary = [
+    {
+        title: 'Lord of the Rings',
+        author: 'J. R. R. Tolkien',
+        dateOfPublication: '1954'
+    },
+    {
+        title: 'The Expanse',
+        author: 'James S. A. Corey',
+        dateOfPublication: '2011'
+    },
+    {
+        title: 'The Foundation',
+        author: 'Isaac Asimov',
+        dateOfPublication: '1942'
+    },
+    { title: 'I Robot', author: 'Isaac Asimov', dateOfPublication: '1950' },
+];
+function Book(title, author, dateOfPublication) {
+    this.title = title;
+    this.author = author;
+    this.dateOfPublication = dateOfPublication;
 }
-
-function addBookToLibrary() {
-  // do stuff here
+var Library = /** @class */ (function () {
+    function Library(library) {
+        this.library = library;
+    }
+    Library.prototype.addBookToLibrary = function (newBook) {
+        this.library.push(newBook);
+    };
+    return Library;
+}());
+function initLibrary() {
+    var newSciFiLibrary = new Library(initialLibrary);
+    var duneBook = new Book('Dune', 'Frank Herbert', '1965');
+    newSciFiLibrary.addBookToLibrary(duneBook);
+    console.log(newSciFiLibrary.library);
+    return newSciFiLibrary;
 }
