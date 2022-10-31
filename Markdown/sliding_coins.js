7b476b38a628d85b91d9e934c874ae330f7824d0
CekanLucas <cekanlucas@gmail.com>
2022-10-31 00:59:45 -0400

    fixed drag ghost image when sliding coins

--- Markdown/sliding_coins.js
+++ Markdown/sliding_coins.js
@@ -1,10 +1,3 @@
-document
-  .getElementById('base-coin-left')
-  .addEventListener('dragstart', dragStart_handler)
-document
-  .getElementById('base-coin-right')
-  .addEventListener('dragstart', dragStart_handler)
-
 // define coin-slots should be able to handle dragged items being dropped there
 const coinSlots = document.querySelectorAll('.active-slot')
 coinSlots.forEach((slot, i) => {
@@ -14,43 +7,50 @@ coinSlots.forEach((slot, i) => {
   el.addEventListener('dragleave', dragLeave_handler)
   el.addEventListener('dragover', dragOver_handler)
   el.addEventListener('drop', drop_handler)
-
+})
+const coins = document.querySelectorAll('.coin')
+coins.forEach((coin, i) => {
+  console.log('coin', coin, i)
+  coin.addEventListener('dragstart', dragStart_handler)
 })
 
 function dragStart_handler(e) {
   // e.preventDefault() //breaks functionality for some reason
   const dt = e.dataTransfer
   e.target.classList.forEach((cl) => {
-    if(cl==='coin-left') dt.setData('coin_type', 'left')
-    if(cl==='coin-right') dt.setData('coin_type', 'right')
-    if(cl==='base-coin') dt.setData('coin_slot', 'base')
+    if (cl === 'coin-left') dt.setData('coin_type', 'left')
+    if (cl === 'coin-right') dt.setData('coin_type', 'right')
+    if (cl === 'base-coin') dt.setData('coin_slot', 'base')
   })
 
-  dt.effectAllowed = 'copy'
-  dt.dropEffect = 'copy'
+  dt.effectAllowed = 'move'
+  dt.dropEffect = 'move'
 
-  dt.setDragImage(e.target, 25, 25);
+  const dragGhostImg = document.getElementById(
+    `base-coin-${dt.getData('coin_type')}`,
+  )
+  dt.setDragImage(dragGhostImg, 25, 25)
 
   console.log('datatransfer', dt.getData('coin_type'))
   console.log('dragstart fired:\t', e, dt.items.length)
 }
-function dragEnter_handler(e) { 
-  e.preventDefault();
+function dragEnter_handler(e) {
+  e.preventDefault()
   const slot = e.target
 
   slot.replaceChildren()
   e.target.appendChild(create_new_coin('blank'))
-  
+
   console.log('dragenter fired:\t', e, e.dataTransfer.items.length)
 }
 function dragLeave_handler(e) {
-  e.preventDefault();
+  e.preventDefault()
   const slot = e.target
   slot.replaceChildren()
   console.log('dragLeave fired:\t', e, e.dataTransfer.items.length)
 }
 function dragOver_handler(e) {
-  e.preventDefault();
+  e.preventDefault()
   const slot = e.target
 
   slot.replaceChildren()
@@ -88,7 +88,7 @@ function create_new_coin(coinType) {
       newCoin.id = `coin_${i}`
       i = null
     }
-    console.log('new coin created of type',newCoin)
+    console.log('new coin created of type', newCoin)
   }
   return newCoin
 }
