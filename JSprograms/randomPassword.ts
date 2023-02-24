c919b4ca057b9fab37f50ceef24933a0f96957c2
CekanLucas <cekanlucas@gmail.com>
2023-02-23 20:45:06 -0500

    random password: refactored to use Typescript

--- JSprograms/randomPassword.ts
+++ JSprograms/randomPassword.ts
@@ -1,15 +1,15 @@
 // It must be at least 8 characters long.
 // It must contain at least one uppercase letter, one lowercase letter, one number, and one special character
 
-function generateRandomPassword(passwordLength = 8) {
-  let hasSpecialChar = false;
-  let hasLowerChar = false;
-  let hasUpperChar = false;
-  let hasNumberChar = false;
+function generateRandomPassword(passwordLength: number = 8): string {
+  let hasSpecialChar: boolean = false;
+  let hasLowerChar: boolean = false;
+  let hasUpperChar: boolean = false;
+  let hasNumberChar: boolean = false;
 
-  let randomPassword = new Array(passwordLength)
+  let randomPassword: string = new Array(passwordLength)
     .fill('', 0, passwordLength)
-    .reduce((val, string) => {
+    .reduce((val: string, string: string) => {
       const char = generateRandomCharacter();
 
       if (!hasSpecialChar && char.group === 'special') hasSpecialChar = true;
@@ -33,7 +33,15 @@ function generateRandomPassword(passwordLength = 8) {
   return randomPassword;
 }
 
-const charRanges = [
+type charGroup = 'special' | 'number' | 'lowercase' | 'uppercase';
+
+interface charRange {
+  0: number; // lower bound of character range
+  1: number; // upper bound of character range
+  2: charGroup; // group that character belongs to
+}
+const charRanges: charRange[] = [
+  //ASCII ranges
   [33, 47, 'special'], // special characters: ! " # $ % & ' ( ) * + , - . /
   [48, 57, 'number'], // number characters: 0 - 9
   [58, 64, 'special'], // special characters: : ; < = > ? @
@@ -43,29 +51,35 @@ const charRanges = [
   [123, 126, 'special'], // special characters: { | } ~
 ];
 
-function generateRandomCharacter() {
+interface character {
+  character: string; // eg '!'
+  group: charGroup; // eg 'special'
+}
+
+function generateRandomCharacter(): character {
   const randomNum = Math.floor(Math.random() * 93) + 33;
-  //ASCII ranges
 
   const charRange = charRanges.find((range) => {
     return randomNum >= range[0] && randomNum <= range[1];
   });
+  if (charRange === undefined) throw 'charRange is empty';
   return { character: String.fromCharCode(randomNum), group: charRange[2] };
 }
 
-function AddRequiredCharacterToString(string, missingGroup) {
+function AddRequiredCharacterToString(string: string, missingGroup: charGroup) {
   const filteredRanges = charRanges.filter((range) => {
     return range[2] === missingGroup;
   });
 
-  const chars = filteredRanges.reduce((chars, range) => {
+  const charCodes: number[] = [];
+  const chars: number[] = filteredRanges.reduce((chars, range) => {
     const [lowerRange, upperRange] = [range[0], range[1]];
 
     for (let i = lowerRange; i < upperRange; i++) {
       chars.push(i);
     }
     return chars;
-  }, []);
+  }, charCodes);
 
   const requiredChar = String.fromCharCode(
     chars[Math.floor(Math.random() * chars.length + 1)]
@@ -79,7 +93,7 @@ function AddRequiredCharacterToString(string, missingGroup) {
   return modifiedString;
 }
 
-function errorHandling(passwordLength) {
+function errorHandling(passwordLength: number): void {
   if (!Number.isFinite(passwordLength))
     console.error('password is not a finite number');
   if (passwordLength < 1) console.error('password is less than 1');
@@ -88,8 +102,6 @@ function errorHandling(passwordLength) {
 console.log(generateRandomPassword());
 
 // Get ASCII values
-'!'.charCodeAt(0); // => 33
+'!'.charCodeAt(0); //?
 // Get char from ASCII code
 String.fromCharCode(50); //?
-
-/* You might want to add some error handling in case the characterLimit parameter is less than 1 or is not a number. For example, you could add a check at the beginning of the generateRandomPassword function to ensure that characterLimit is a positive number. */
