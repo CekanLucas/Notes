9abb84585c6b905b1ff6c143026460088c7fee6b
CekanLucas <cekanlucas@gmail.com>
2023-04-02 02:18:41 -0400

    sphinx: configuration

--- Markdown/notebook/sphinx/source/conf.py
+++ Markdown/notebook/sphinx/source/conf.py
@@ -28,8 +28,12 @@ author = 'Lucas Cekan'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-  'myst_parser' # use myST instead of reST
+  'myst_parser', # use myST instead of reST
+  'sphinx_rtd_dark_mode' # toggle-able dark mode
 ]
+myst_enable_extensions = ['deflist','colon_fence', 'dollarmath', 'linkify', 'substitution', 'tasklist']
+# user starts in dark mode
+default_dark_mode = True
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -45,7 +49,7 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_book_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
