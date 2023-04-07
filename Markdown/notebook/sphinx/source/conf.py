44159c63818be491b6091a812ac6f42fb3b7a53f
CekanLucas <cekanlucas@gmail.com>
2023-04-07 19:17:15 -0400

    sphinx: static assets js scripts and css in _static dir

--- Markdown/notebook/sphinx/source/conf.py
+++ Markdown/notebook/sphinx/source/conf.py
@@ -83,3 +83,7 @@ html_theme = 'sphinx_book_theme'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+# paths relative to _static
+html_js_files = ['custom_script.js']
+html_css_files = ['style.css']
